d882f83e398269b30faa6f8e0055f982c6dea860
rusconn <rusconn3@gmail.com>
2026-05-10 22:07:47 +0900

    test(cli): add tests for writers

--- cli/src/commands/breakpoints/writers/plain.rs
+++ cli/src/commands/breakpoints/writers/plain.rs
@@ -23,3 +23,32 @@ pub(in super::super) fn write(
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::num::NonZeroUsize;
+
+    #[test]
+    fn test_plain_writer() {
+        let mut map = BTreeMap::new();
+        let e1 = Enemy {
+            name: "goblin".to_string(),
+            hp: NonZeroUsize::new(10).unwrap(),
+            def: 5,
+        };
+        let e2 = Enemy {
+            name: "slime".to_string(),
+            hp: NonZeroUsize::new(5).unwrap(),
+            def: 2,
+        };
+        map.insert(10, vec![&e1]);
+        map.insert(15, vec![&e1, &e2]);
+
+        let mut buf = Vec::new();
+        write(&mut buf, &map).unwrap();
+
+        let output = String::from_utf8(buf).unwrap();
+        assert_eq!(output, "10: goblin\n15: goblin,slime");
+    }
+}
