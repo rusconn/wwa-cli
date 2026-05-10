1346a107f7ed3bc616a1707d23fac18bae33906e
rusconn <rusconn3@gmail.com>
2026-05-10 23:14:54 +0900

    refactor(cli): use rstest

--- cli/src/commands/breakpoints/writers.rs
+++ cli/src/commands/breakpoints/writers.rs
@@ -25,35 +25,54 @@ impl Format {
 mod tests {
     use std::num::NonZeroUsize;
 
+    use rstest::{fixture, rstest};
+
     use super::*;
 
-    #[test]
-    fn test_writers() {
-        let mut map = BTreeMap::new();
-        let goblin = Enemy {
+    #[fixture]
+    #[once]
+    fn goblin() -> Enemy {
+        Enemy {
             name: "goblin".to_string(),
             hp: NonZeroUsize::new(10).unwrap(),
             def: 5,
-        };
-        let slime = Enemy {
+        }
+    }
+
+    #[fixture]
+    #[once]
+    fn slime() -> Enemy {
+        Enemy {
             name: "slime".to_string(),
             hp: NonZeroUsize::new(5).unwrap(),
             def: 2,
-        };
-        map.insert(10, vec![&goblin]);
-        map.insert(15, vec![&goblin, &slime]);
-
-        let format_cases: [(Format, &str); _] = [
-            (Format::Plain, "10: goblin\n15: goblin,slime"),
-            (Format::Json, r#"{"10":["goblin"],"15":["goblin","slime"]}"#),
-        ];
-
-        for (writer, expect) in format_cases {
-            let mut buf = Vec::new();
-            writer.write(&mut buf, &map).unwrap();
-
-            let output = String::from_utf8(buf).unwrap();
-            assert_eq!(output, *expect);
         }
+    }
+
+    #[fixture]
+    #[once]
+    fn map(
+        goblin: &'static Enemy,
+        slime: &'static Enemy,
+    ) -> BTreeMap<Breakpoint, Vec<&'static Enemy>> {
+        let mut map = BTreeMap::new();
+        map.insert(10, vec![goblin]);
+        map.insert(15, vec![goblin, slime]);
+        map
+    }
+
+    #[rstest]
+    #[case(Format::Plain, "10: goblin\n15: goblin,slime")]
+    #[case(Format::Json, r#"{"10":["goblin"],"15":["goblin","slime"]}"#)]
+    fn test_format_writers(
+        map: &BTreeMap<Breakpoint, Vec<&'static Enemy>>,
+        #[case] format: Format,
+        #[case] expect: &str,
+    ) {
+        let mut buf = Vec::new();
+        format.write(&mut buf, map).unwrap();
+
+        let output = String::from_utf8(buf).unwrap();
+        assert_eq!(output, *expect);
     }
 }
